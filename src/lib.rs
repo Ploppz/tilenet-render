45361c5e58c69bc6a4091d843b68927d224d8e1b
Erlend Hofstad Langseth <3rlendhl@gmail.com>
2016-12-17 22:05:14 +0100

    re-export filter enums in root

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,5 @@ extern crate tile_net;
 
 pub mod renderer;
 pub use renderer::Renderer as Ren;
+pub use renderer::MinifySamplerFilter as MinifySamplerFilter;
+pub use renderer::MagnifySamplerFilter as MagnifySamplerFilter;
